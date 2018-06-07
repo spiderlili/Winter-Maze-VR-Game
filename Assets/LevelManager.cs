62b2585d90792c7a6c17bd3bbe65ad52050bdd52
Jing Chun Tan <jtan1993@gmail.com>
2018-06-07 17:24:12 +0100

    backup

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public void LoadMainLevel() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoWalk : MonoBehaviour {
+    public float speed = 1.0f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Snowman")) {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
